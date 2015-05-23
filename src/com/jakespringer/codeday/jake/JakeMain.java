941d646ea9eecb46f7fc364b52d43fc9abbac993
Jake Springer <me@jakespringer.com>
2015-05-23 08:31:11 -0700

    networking backend added

--- src/com/jakespringer/codeday/jake/JakeMain.java
+++ src/com/jakespringer/codeday/jake/JakeMain.java
@@ -1,7 +1,6 @@
 package com.jakespringer.codeday.jake;
 
 import com.jakespringer.codeday.jake.netinterface.NetworkSystem;
-import com.jakespringer.codeday.jake.networking.ClientServerConnection;
 import com.jakespringer.codeday.testgame.OtherRed;
 import com.jakespringer.codeday.testgame.Red;
 import com.jakespringer.engine.core.Main;
@@ -15,13 +14,13 @@ public abstract class JakeMain {
     public static void main(String[] args) throws IOException {
         System.setProperty("org.lwjgl.librarypath", new File("natives").getAbsolutePath());
         try {
-    		ClientServerConnection c = new ClientServerConnection("127.0.0.1", 1337);
-    		c.setDaemon(true);
-    		c.connect();
-    		c.start();
+//    		ClientServerConnection c = new ClientServerConnection("127.0.0.1", 1337);
+//    		c.setDaemon(true);
+//    		c.connect();
+//    		c.start();
             Main.init();
             new OtherRed(new Vec2(0, 0));
-            Main.gameManager.add(new NetworkSystem(c, Main.gameManager.elc.getEntity(Red.class), Main.gameManager.elc.getEntity(OtherRed.class)));
+//            Main.gameManager.add(new NetworkSystem(c, Main.gameManager.elc.getEntity(Red.class), Main.gameManager.elc.getEntity(OtherRed.class)));
             Main.run();
         } catch (Exception ex) {
             ex.printStackTrace();
--- src/com/jakespringer/codeday/jake/netinterface/NetworkSystem.java
+++ src/com/jakespringer/codeday/jake/netinterface/NetworkSystem.java
@@ -7,7 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
 
-import com.jakespringer.codeday.jake.networking.ClientServerConnection;
 import com.jakespringer.codeday.testgame.OtherRed;
 import com.jakespringer.codeday.testgame.Red;
 import com.jakespringer.engine.core.AbstractSystem;
@@ -16,41 +15,41 @@ import com.jakespringer.engine.util.Vec2;
 
 public class NetworkSystem extends AbstractSystem {
 	
-	private ClientServerConnection conn;
+//	private ClientServerConnection conn;
 	private Red you;
 	private OtherRed other;
-
-	public NetworkSystem(ClientServerConnection c, Red y, OtherRed o) {
-		conn = c;
-		you = y;
-		other = o;
-	}
+//
+//	public NetworkSystem(ClientServerConnection c, Red y, OtherRed o) {
+////		conn = c;
+//		you = y;
+//		other = o;
+//	}
 	
 	@Override
 	public void update() {
-		byte[] msg;
-		try {
-			msg = conn.next();
-			if (msg != null) System.out.println(msg.length);
-			if (msg != null && msg.length == 17) {
-//				if (msg.startsWith("~")) {
-					ByteBuffer b = ByteBuffer.allocate(32);
-					b.put(msg);
-					System.out.println("Recv: " + b.get() + " " + b.get() + " " +  b.get() + " " + b.get() + " " + b.get() + " " +b.get() + " " +b.get() + " " +b.get() + ";");
-					b.position(0);
-					other.getComponent(PositionComponent.class).pos = new Vec2(b.getDouble(), b.getDouble());
-//				}
-			}
-			ByteBuffer b = ByteBuffer.allocate(16);
-			Vec2 p = you.getComponent(PositionComponent.class).pos;
-			b.putDouble(p.x);
-			b.putDouble(p.y);
-			b.flip();
-//			System.out.println("Sent: " + b.get() + " " + b.get() + " " +  b.get() + " " + b.get() + " " + b.get() + " " +b.get() + " " +b.get() + " " +b.get() + ";");
-			b.flip();
-			conn.send(b.array());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		byte[] msg;
+//		try {
+//			msg = conn.next();
+//			if (msg != null) System.out.println(msg.length);
+//			if (msg != null && msg.length == 17) {
+////				if (msg.startsWith("~")) {
+//					ByteBuffer b = ByteBuffer.allocate(32);
+//					b.put(msg);
+//					System.out.println("Recv: " + b.get() + " " + b.get() + " " +  b.get() + " " + b.get() + " " + b.get() + " " +b.get() + " " +b.get() + " " +b.get() + ";");
+//					b.position(0);
+//					other.getComponent(PositionComponent.class).pos = new Vec2(b.getDouble(), b.getDouble());
+////				}
+//			}
+//			ByteBuffer b = ByteBuffer.allocate(16);
+//			Vec2 p = you.getComponent(PositionComponent.class).pos;
+//			b.putDouble(p.x);
+//			b.putDouble(p.y);
+//			b.flip();
+////			System.out.println("Sent: " + b.get() + " " + b.get() + " " +  b.get() + " " + b.get() + " " + b.get() + " " +b.get() + " " +b.get() + " " +b.get() + ";");
+//			b.flip();
+//			conn.send(b.array());
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
